0de5fa8fb1c923321c6100917e97cbdee4d3f1c1
Michael Weinrich <michael.weinrich@internations.org>
2013-08-23 00:39:44 +0200

    Initial commmit of skeleton created by Berkshelf

---
+++ README.md
@@ -0,0 +1,13 @@
+# exim4 cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Michael Weinrich (<micxer@micxer.de>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'exim4'
+maintainer       'Michael Weinrich'
+maintainer_email 'micxer@micxer.de'
+license          'Apache 2.0'
+description      'Installs/Configures exim4'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
