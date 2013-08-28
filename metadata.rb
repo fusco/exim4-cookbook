66c8241e968480229168448e77e4d8f8f31120a6
Michael Weinrich <michael.weinrich@internations.org>
2013-08-28 02:32:02 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'micxer@micxer.de'
 license          'Apache 2.0'
 description      'Installs/Configures exim4'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
 recipe           'exim4', 'Installs and configures exim4'
 
 %w{debian}.each do |os|
